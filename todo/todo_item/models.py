d6526e0a0f38cb1887b1769c4ee42e2d1e14af6b
dmitriymitr <imfitrit@mail.ru>
2020-12-20 13:35:24 +0300

    Homework3+4
    Hw 3
       debug 'debug.py'
    
    Hw 4
       Change list.html with master.html
       todo_item make DB and change view, models, admin

--- todo/todo_item/models.py
+++ todo/todo_item/models.py
@@ -8,8 +8,7 @@ class ItemModel(models.Model):
     name = models.CharField(max_length=128, verbose_name='Изменение списка')
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    list_model = models.CharField(max_length=256, verbose_name='Список')
+    list_model = models.ForeignKey('main.List', on_delete=models.CASCADE)
     is_done = models.BooleanField(default=False)
     expare_date = models.DateTimeField(blank=True)
 
